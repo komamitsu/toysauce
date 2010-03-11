e6b504bccc05a41d391c0b75e99607b2b32c5be3
komamitsu <komamitsu@gmail.com>
2010-03-11 18:11:50 +0900

    add debug option.

--- main.ml
+++ main.ml
@@ -140,15 +140,24 @@ and innar_func env params name =
   | _ -> 
       failwith (sprintf "innar_func: %s was not found" name)
 
+let debug = ref false
+let source = ref stdin
+
 let _ =
+  let args_spec = [
+    "-g", Arg.Set debug, "for debugging";
+  ] in
+  Arg.parse args_spec (fun file -> source := open_in file)
+    (sprintf "usage: %s [options] [programfile]" Sys.argv.(0));
   Printexc.record_backtrace true;
   try 
-    let lexbuf = Lexing.from_channel stdin in
+    let lexbuf = Lexing.from_channel !source in
     let rec loop parsed =
       let result = Parser.main Lexer.token lexbuf in
       result::parsed in
     let expr_list = loop [] in
-    print_endline (string_of_expr (ExprList expr_list)); flush stdout;
+    if !debug 
+    then print_endline (string_of_expr (ExprList expr_list)); flush stdout;
     ignore (eval_expr (empty_env ()) (ExprList expr_list))
   with e -> 
     Printf.eprintf "Unexpected exception : %s\n" (Printexc.to_string e);
